8ecee39f6aa01a20cc4aeff31fcc6164edc2ca18
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-05-12 11:15:45 -0700

    imported kIsWeb

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 
 void main() async {
